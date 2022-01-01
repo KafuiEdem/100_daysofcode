df81c558d9bb16754d343ab8a0b560f4732c4e5c
KafuiEdem <robinedemamedzo@gmail.com>
2022-01-01 23:23:34 +0000

    adding day1 final project

---
+++ DAY1/project_day1/README.md
@@ -0,0 +1,13 @@
+   ## BAND NAME GENERATOR
+    This project helps to generate a band name for you.<br>
+    It first ask of the city your grew up in, and <br>
+    also asked for your pet's name. After that the program <br>
+    combines the city and pet name to generate your band name.<br>
+
+THIS IS A CONSOLE GAME
+
+
+
+ ![](one.png)
+
+ ![](two.png)
--- DAY1/project_day1/band_name_generator.py
+++ DAY1/project_day1/band_name_generator.py
@@ -1,4 +1,14 @@
 """
     This project helps to generate a band name for you.
+    It first ask of the city your grew up in, and 
+    also asked for your pet's name. After that the program
+    combines the city and pet name to generate your band name.
     
 """
+banner = "Welcome to the Band Name Generator."
+print(banner)
+city = input("What's name of the city our grew up in?\n")
+pet = input("What's your pet's name?\n")
+name_generate = "Your band could be "+ city +" "+pet 
+
+print(name_generate)
